ed12c60ed2311a8ff59ff79b6f5bd2699a981d74
Jacob Marshall <git@jacobmarshall.co>
2017-05-27 23:38:06 +1200

    Add an experimental cli

---
+++ cli.js
@@ -0,0 +1,82 @@
+const meow = require('meow');
+const Listr = require('listr');
+const rc = require('rc');
+const { upload } = require('./');
+
+const cli = meow(`
+    Usage
+      $ bugsnag-sourcemaps upload
+
+    Options
+      -h, --help                 Prints this message
+      -k, --api-key KEY          Your project API key
+      -v, --app-version VERSION  The version number of your app
+      -c, --code-bundle-id ID    The code bundle id (react-native only)
+      -u, --minified-url URL     The URL your users will request your bundle
+      -s, --source-map PATH      The path of the source map file (local)
+      -p, --minified-file PATH   The path of the bundle (local)
+      -u, --upload-sources BOOL  Upload source files referenced by the source map
+      -r, --project-root PATH    The root path to remove from absolute file paths
+      -o, --overwrite BOOL       Overwite previously uploaded source maps
+
+    Examples
+      $ bugsnag-sourcemaps upload \\
+          --api-key f915102cdb8153ee934b8549c930aa1b \\
+          --app-version 1.0.0 \\
+          --minified-url https://cdn.example.com/dist/bundle.js \\
+          --source-map dist/bundle.js.map \\
+          --minified-file dist/bundle.js
+      OR
+      $ bugsnag-sourcemaps upload \\
+          --api-key f915102cdb8153ee934b8549c930aa1b \\
+          --code-bundle-id 1.0-123 \\
+          --minified-url main.jsbundle \\
+          --source-map dist/main.jsbundle.map \\
+          --minified-file dist/main.jsbundle \\
+          --upload-sources true
+`, {
+    alias: {
+        k: 'api-key',
+        v: 'app-version',
+        c: 'code-bundle-id',
+        u: 'minified-url',
+        s: 'source-map',
+        p: 'minified-file',
+        u: 'upload-sources',
+        r: 'project-root',
+        o: 'overwrite',
+    },
+    // minimistOptions: {
+    //     string: [
+    //         'app-version',
+    //     ],
+    // },
+});
+
+const conf = {
+    // Any cli-specific defaults (none currently)
+};
+
+// Pull configuration from a local .bugsnagrc file
+Object.assign(conf, rc('bugsnag').sourcemaps);
+
+// Then extract any overrides from the flags
+Object.assign(conf, cli.flags);
+
+for (const key in conf) {
+    // Strip out the single letter (aliases) from meow
+    if (key.length === 1) {
+        delete conf[key];
+    }
+}
+
+const tasks = new Listr([
+    {
+        title: 'Uploading sourcemaps',
+        task: () => upload(conf),
+    },
+]);
+
+tasks.run().catch(err => {
+    // console.error(err);
+});
